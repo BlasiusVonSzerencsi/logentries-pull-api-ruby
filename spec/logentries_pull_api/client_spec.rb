b9fb85930a5468c6e86241d2154f8500331bcbfd
Balazs Szerencsi <balazs.szerencsi@icloud.com>
2016-08-05 15:21:07 +0200

    Small refactor.

--- spec/logentries_pull_api/client_spec.rb
+++ spec/logentries_pull_api/client_spec.rb
@@ -30,23 +30,24 @@ describe LogentriesPullApi::Client do
     let(:uri) { instance_double URI::HTTPS }
     let(:response) { instance_double Net::HTTPResponse }
 
+    before do
+      allow(URI).to receive(:parse).and_return uri
+      allow(uri).to receive(:query=)
+      allow(Net::HTTP).to receive(:get_response).with(uri).and_return response
+      allow(response).to receive(:body).and_return '[]'
+    end
+
     it 'should assemble a proper LogEntries API URL' do
       expect(URI).to receive(:parse)
                          .with('https://pull.logentries.com/my_account_key/hosts/my_log_set_key/my_log_key/')
                          .and_return uri
-      allow(uri).to receive(:query=)
-      allow(Net::HTTP).to receive(:get_response).with(uri).and_return response
-      allow(response).to receive(:body).and_return '{}'
 
       subject.get
     end
 
 
     it 'should request logs in JSON format' do
-      allow(URI).to receive(:parse).and_return uri
       expect(uri).to receive(:query=).with 'format=json'
-      allow(Net::HTTP).to receive(:get_response).with(uri).and_return response
-      allow(response).to receive(:body).and_return '{}'
 
       subject.get
     end
@@ -58,51 +59,39 @@ describe LogentriesPullApi::Client do
       expect(URI).to receive(:parse)
                          .with(/my%20log%20set%20key%20with%20whitespaces\/log%20key%20with%20whitespaces/)
                          .and_return uri
-      allow(uri).to receive(:query=)
-      allow(Net::HTTP).to receive(:get_response).and_return response
-      allow(response).to receive(:body).and_return '{}'
 
       subject.get
     end
 
 
     it 'should make a HTTP request to the assembled URL' do
-      allow(URI).to receive(:parse).and_return uri
-      allow(uri).to receive(:query=)
       expect(Net::HTTP).to receive(:get_response).with(uri).and_return response
-      allow(response).to receive(:body).and_return '{}'
 
       subject.get
     end
 
 
     it 'should return the parsed entries' do
-      allow(URI).to receive(:parse).and_return uri
-      allow(uri).to receive(:query=)
-      allow(Net::HTTP).to receive(:get_response).with(uri).and_return response
       allow(response).to receive(:body).and_return '[{"some_key":"some_value"},{"another_key":"another_value"}]'
 
       expect(subject.get).to eq [{'some_key' => 'some_value'}, {'another_key' => 'another_value'}]
     end
 
+    context 'when called with parameters' do
 
-    it 'should allow filtering result set with time range' do
-      allow(URI).to receive(:parse).and_return uri
-      expect(uri).to receive(:query=).with /start=1432080000000&end=1432092600000/
-      allow(Net::HTTP).to receive(:get_response).with(uri).and_return response
-      allow(response).to receive(:body).and_return '{}'
+      it 'should allow filtering result set with time range' do
+        expect(uri).to receive(:query=).with /start=1432080000000&end=1432092600000/
 
-      subject.get start: 1432080000000, end: 1432092600000
-    end
+        subject.get start: 1432080000000, end: 1432092600000
+      end
 
 
-    it 'should allow limiting result set size' do
-      allow(URI).to receive(:parse).and_return uri
-      expect(uri).to receive(:query=).with /limit=123/
-      allow(Net::HTTP).to receive(:get_response).with(uri).and_return response
-      allow(response).to receive(:body).and_return '{}'
+      it 'should allow limiting result set size' do
+        expect(uri).to receive(:query=).with /limit=123/
 
-      subject.get limit: 123
+        subject.get limit: 123
+      end
+
     end
 
   end
